d8792b46b9647acf6df5459692be05a31da48572
diepphan <ngocdiepphan180402@gmail.com>
2024-06-04 17:31:32 +0700

    Implement example of learn updating array in state #28

--- my-app/src/components/AddInteract/StateArray.jsx
+++ my-app/src/components/AddInteract/StateArray.jsx
@@ -1,52 +1,59 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
 ];
 
 export default function StateArray() {
-  const [list, setList] = useState(
-    initialList
-  );
+  const [list, setList] = useState(initialList);
 
   function handleToggle(artworkId, nextSeen) {
-    setList(list.map(artwork => {
-      if (artwork.id === artworkId) {
-        return { ...artwork, seen: nextSeen };
-      } else {
-        return artwork
-      }
-    }
-    ))
+    setList(
+      list.map((artwork) => {
+        if (artwork.id === artworkId) {
+          return { ...artwork, seen: nextSeen };
+        } else {
+          return artwork;
+        }
+      })
+    );
   }
 
   return (
     <>
-    <h1>Art Bucket List</h1>
-    <h2>My list of art to see:</h2>
-    <ItemList
-        artworks={list}
-        onToggle={handleToggle} />
+      <h3>Art Bucket List</h3>
+      <p>My list of art to see:</p>
+      <ItemList artworks={list} onToggle={handleToggle} />
+
+      <section>
+        <h3 style={{ color: "blue" }}>Example adding to an array</h3>
+        <List />
+        <h3 style={{ color: "blue" }}>Transforming an array</h3>
+        <ShapeEditor />
+        <h3 style={{ color: "blue" }}>Replacing items in an array</h3>
+        <CounterList />
+        <h3 style={{ color: "blue" }}>Inserting into an array</h3>
+        <InsertArray />
+        <h3 style={{ color: "blue" }}>Object inside array</h3>
+        <ObjectInsideArray />
+      </section>
     </>
-  )
+  );
 }
 
 function ItemList({ artworks, onToggle }) {
   return (
     <ul>
-      {artworks.map(artwork => (
+      {artworks.map((artwork) => (
         <li key={artwork.id}>
           <label>
             <input
               type="checkbox"
               checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked);
               }}
             />
             {artwork.title}
@@ -56,3 +63,268 @@ function ItemList({ artworks, onToggle }) {
     </ul>
   );
 }
+
+let nextId = 0;
+function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  return (
+    <>
+      <h3>Inspiring sculptors:</h3>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          setArtists([...artists, { id: nextId++, name: name }]);
+        }}
+      >
+        Add
+      </button>
+      <ul style={{ color: "green" }}>
+        {artists.map((artist) => (
+          <li key={artist.id}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                setArtists(artists.filter((a) => a.id !== artist.id));
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+let initialShapes = [
+  { id: 0, type: "circle", x: 50, y: 100 },
+  { id: 1, type: "square", x: 150, y: 100 },
+  { id: 2, type: "circle", x: 250, y: 100 },
+];
+
+function ShapeEditor() {
+  const [shapes, setShapes] = useState(initialShapes);
+
+  function handleClick() {
+    const nextShapes = shapes.map((shape) => {
+      if (shape.type === "square") {
+        // No change
+        return shape;
+      } else {
+        // Return a new circle 50px below
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    // Re-render with the new array
+    setShapes(nextShapes);
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>Move circles down!</button>
+      {shapes.map((shape) => (
+        <div
+          key={shape.id}
+          style={{
+            background: "purple",
+            position: "absolute",
+            left: shape.x,
+            top: shape.y,
+            borderRadius: shape.type === "circle" ? "50%" : "",
+            width: 20,
+            height: 20,
+          }}
+        />
+      ))}
+    </>
+  );
+}
+
+let initialCounters = [0, 0, 0];
+
+function CounterList() {
+  const [counters, setCounters] = useState(initialCounters);
+
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setCounters(nextCounters);
+  }
+
+  return (
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button
+            onClick={() => {
+              handleIncrementClick(i);
+            }}
+          >
+            +1
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+const initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
+
+function InsertArray() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState(initialArtists);
+  const [list, setList] = useState(initialList);
+
+  function handleClick() {
+    const insertAt = 1;
+    const nextArtists = [
+      ...artists.slice(0, insertAt),
+      { id: nextId++, name: name },
+      ...artists.slice(insertAt),
+    ];
+    setArtists(nextArtists);
+    setName("");
+  }
+
+  function handleReversClick() {
+    const nextList = [...list];
+    nextList.reverse();
+    setList(nextList);
+  }
+
+  return (
+    <>
+      <h3>Inspiring sculptors:</h3>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={handleClick}>Insert</button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleReversClick}>
+        Reverse
+      </button>
+      <ul>
+        {list.map(artwork => (
+          <li key={artwork.id}>{artwork.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+
+const BucketList = [
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
+];
+
+function ObjectInsideArray() {
+  const [myList, setMyList] = useState(initialList);
+  const [yourList, setYourList] = useState(
+    initialList
+  );
+  /*
+  function handleToggleMyList(artworkId, nextSeen) {
+    const myNextList = [...myList];
+    const artwork = myNextList.find(
+      a => a.id === artworkId
+    );
+    artwork.seen = nextSeen;
+    setMyList(myNextList);
+  }
+
+  function handleToggleYourList(artworkId, nextSeen) {
+    const yourNextList = [...yourList];
+    const artwork = yourNextList.find(
+      a => a.id === artworkId
+    );
+    artwork.seen = nextSeen;
+    setYourList(yourNextList);
+  }
+  */
+
+   function handleToggleMyList(artworkId, nextSeen) {
+    setMyList(myList.map(artwork => {
+      if (artwork.id === artworkId) {
+        // Create a *new* object with changes
+        return { ...artwork, seen: nextSeen };
+      } else {
+        // No changes
+        return artwork;
+      }
+    }));
+  }
+
+  function handleToggleYourList(artworkId, nextSeen) {
+    setYourList(yourList.map(artwork => {
+      if (artwork.id === artworkId) {
+        // Create a *new* object with changes
+        return { ...artwork, seen: nextSeen };
+      } else {
+        // No changes
+        return artwork;
+      }
+    }));
+  }
+
+  return (
+    <>
+      <h1>Art Bucket List</h1>
+      <h4>My list of art to see:</h4>
+      <ItemList
+        artworks={myList}
+        onToggle={handleToggleMyList} />
+      <h4>Your list of art to see:</h4>
+      <ItemList
+        artworks={yourList}
+        onToggle={handleToggleYourList} />
+    </>
+  );
+
+  function ItemList({ artworks, onToggle }) {
+    return (
+      <ul>
+        {artworks.map(artwork => (
+          <li key={artwork.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={artwork.seen}
+                onChange={e => {
+                  onToggle(
+                    artwork.id,
+                    e.target.checked
+                  );
+                }}
+              />
+              {artwork.title}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+
