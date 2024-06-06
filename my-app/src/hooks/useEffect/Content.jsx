9a6f2015d06b8b59729fed9e220a01687c17645c
diepphan <ngocdiepphan180402@gmail.com>
2024-06-06 10:39:37 +0700

    Implement example call api useEffect #34

--- my-app/src/hooks/useEffect/Content.jsx
+++ my-app/src/hooks/useEffect/Content.jsx
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react";
 
+const tabs = ["posts", "comments", "albums"];
+
 export default function Content() {
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
+  const [type, setType] = useState("posts");
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    console.log(type); // Sử dụng trực tiếp console.log
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((res) => res.json())
       .then((posts) => {
         setPosts(posts);
       });
-  }, []);
+  }, [type]);
 
   return (
     <>
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          style={
+            type === tab ? {
+              color: "green",
+              backgroundColor: "violet" } : {}
+          }
+          onClick={() => setType(tab)}
+        >
+          {tab}
+        </button>
+      ))}
       <input value={title} onChange={(e) => setTitle(e.target.value)} />
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>{post.title || post.name}</li>
         ))}
       </ul>
     </>
