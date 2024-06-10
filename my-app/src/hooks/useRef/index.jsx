a15603606080012810f258a7259ff17dcb1eb116
diepphan <ngocdiepphan180402@gmail.com>
2024-06-10 14:53:41 +0700

    Implement example useRef for DOM #43

--- my-app/src/hooks/useRef/index.jsx
+++ my-app/src/hooks/useRef/index.jsx
@@ -1,22 +1,31 @@
 import Count from "./Count";
 import Counter from "./Counter";
 import Stopwatch from "./Stopwatch";
+import Form  from "./Focus"
+import CatFriends from "./Scroll";
+import VideoPlayer from "./Player";
 
 export default function UseRef() {
   return (
     <>
-    <section>
-      <h1 className="title">Example useRef for count down</h1>
-      <Count />
-    </section>
-    <section>
-    <h1 className="title">Example useRef for stop watch</h1>
-    <Stopwatch />
-    <section>
-    <h1 className="title">Example useRef for click counter</h1>
-    <Counter />
-  </section>
-  </section>
-  </>
+      <section>
+        <h1 className="title">Example useRef for count down</h1>
+        <Count />
+      </section>
+      <section>
+        <h1 className="title">Example useRef for stop watch</h1>
+        <Stopwatch />
+      </section>
+      <section>
+        <h1 className="title">Example useRef for click counter</h1>
+        <Counter />
+      </section>
+      <section>
+        <h1 className="title">Example useRef for DOM</h1>
+        <Form />
+        <CatFriends />
+        <VideoPlayer />
+      </section>
+    </>
   );
 }
