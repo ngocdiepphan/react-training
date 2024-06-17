3c272f006f2239d0a6db0f1fabc472834736b6cc
diepphan <ngocdiepphan180402@gmail.com>
2024-06-17 10:53:05 +0700

    Implement create file for practice #58

---
+++ practice/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+        <h1 className="text-2xl font-bold text-gray-900">Hello Tailwind CSS!</h1>
+        <p className="text-gray-600">This is a simple example of using Tailwind CSS in a React project.</p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
