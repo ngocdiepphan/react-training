6b62dcab3791754cc76825af56d5945342494a10
diepphan <ngocdiepphan180402@gmail.com>
2024-06-17 15:02:45 +0700

    Implement setup variable css #58

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
-        <h1 className="text-2xl font-bold text-red-600">Hello Tailwind CSS!</h1>
-        <p className="text-blue-500 text-xl">This is a simple example of using Tailwind CSS in a React project.</p>
+      <h1 className="text-2xl font-bold text-red-600 bg-green-200 md:text-secondary md:bg-blue-500 lg:text-green-500 lg:bg-bgSecondary">
+          Hello Tailwind CSS!
+        </h1>
+        <p className="text-blue-500 text-xl">
+          This is a simple example of using Tailwind CSS in a React project.
+        </p>
+        <h1 className="font-sans text-lg md:text-xl lg:text-2xl">
+          Hello, Inter-Regular!
+        </h1>
+        <p className="font-serif text-base md:text-lg lg:text-xl">
+          This is Playfair Display-Regular.
+        </p>
       </div>
     </div>
   );
--- practice/tailwind.config.js
+++ practice/tailwind.config.js
@@ -4,7 +4,43 @@ module.exports = {
     './src/**/*.{html,js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+    },
+    colors: {
+      primary: '#000',      
+      secondary: '#7f7f7f', 
+      bgPrimary: '#fff',    
+      bgSecondary: '#ff642b'
+    },
+    // fontSize: {
+    //   'x-small': '12px',
+    //   'small': '14px',
+    //   'x-medium': '16px',
+    //   'medium': '17px',
+    //   'x-large': '18px',
+    //   'large': '20px',
+    //   'xl-large': '23px',
+    //   'xxl-large': '30px',
+    //   'x-huge': '36px',
+    //   'huge': '45px',
+    //   'xl-huge': '50px',
+    //   'xxl-huge': '76px',
+    // },
+    // lineHeight: {
+    //   'x-small': '1.2',
+    //   'x-medium': '1.5',
+    //   'medium': '1.56',
+    //   'xs-medium': '1.58',
+    // },
+    // screens: {
+    //   'desktop': '1200px',
+    //   'tablet': '768px',
+    //   'mobile': '480px',
+    // },
   },
   plugins: [],
 };
