30ff68901a25f63282e68d57537c7dd80e6a7468
diepphan <ngocdiepphan180402@gmail.com>
2024-06-17 13:54:30 +0700

    Implement install tailwind #58

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import './index.css';
 
 function App() {
   return (
-    <>
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
-        <h1 className="text-2xl font-bold text-gray-900">Hello Tailwind CSS!</h1>
-        <p className="text-gray-600">This is a simple example of using Tailwind CSS in a React project.</p>
+        <h1 className="text-2xl font-bold text-red-600">Hello Tailwind CSS!</h1>
+        <p className="text-blue-500 text-xl">This is a simple example of using Tailwind CSS in a React project.</p>
       </div>
     </div>
-    </>
   );
 }
 
