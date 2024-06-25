49e709992d8a0ab08fe7645a6466125b795fa4a7
diepphan <ngocdiepphan180402@gmail.com>
2024-06-25 22:15:35 +0700

    Implement create component load more #112

---
+++ practice/src/components/Inputs/LoadMore/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoadMore: React.FC = () => {
+  return (
+    <>
+      <div className="font-sans cursor-pointer rounded-md flex justify-center items-center w-124 h-32 mt-30 mx-auto mb-96 hover:bg-blue-200 border border-gray-700">
+        Load More
+      </div>
+    </>
+  );
+};
+export default LoadMore;
