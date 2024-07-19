f7b8d6eb7ede8ef6ace03ed4a844cc33e19147d1
diepphan <ngocdiepphan180402@gmail.com>
2024-07-19 17:16:54 +0700

    Implement create mock data for component tips recipes in recipe-detail #235

---
+++ practice/src/mocks/tipsRecipe.ts
@@ -0,0 +1,43 @@
+export const tipRecipes = [
+  {
+    id:"1",
+    img: "https://github.com/user-attachments/assets/c956656d-8256-49b0-af32-f2065f1453ce",
+    name: "Cranberry Macaroon Ice Cream Cake"
+  },
+  {
+    id:"2",
+    img: "https://github.com/user-attachments/assets/cddd6f06-5a51-43b7-8b99-f393328ad6ed",
+    name: "No Bake Cheesecake"
+  },
+  {
+    id:"3",
+    img: "https://github.com/user-attachments/assets/9c00ce5e-a37f-4757-81ed-03c8a6e834c9",
+    name: "Double Thick Layered Sponge Cake"
+  },
+  {
+    id:"4",
+    img: "https://github.com/user-attachments/assets/bec21c3b-2c33-44ab-8944-40aef7e7c1fc",
+    name: "Caramel Glaze Cake"
+  },
+  {
+    id:"5",
+    img: "(https://github.com/user-attachments/assets/daa206f9-239f-4035-83af-eaa442b5fce2",
+    name: "Strawberry Cream Cake Bites"
+  },
+  {
+    id:"6",
+    img: "https://github.com/user-attachments/assets/688e4782-836a-467f-bcd4-79373a59e0ec",
+    name: "Homemade Mixed Berries Wedding Cake"
+  },
+  {
+    id:"7",
+    img: "(https://github.com/user-attachments/assets/52a15847-b5b1-4546-90cd-5a7c4ce366ed",
+    name: "Homemade Mixed Berries Wedding Cake"
+  },
+  {
+    id:"8",
+    img: "https://github.com/user-attachments/assets/87a79710-33d5-4e40-a9e5-5469d573f5fa",
+    name: "Almond Cinnamon Sponge Cake"
+  },
+
+]
