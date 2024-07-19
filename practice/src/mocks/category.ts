5f56711a600eb0f1c6062c76ebcb40b42cc619b2
diepphan <ngocdiepphan180402@gmail.com>
2024-07-19 16:59:54 +0700

    Implement update data recipe in db.json and mock #235

---
+++ practice/src/mocks/category.ts
@@ -0,0 +1,32 @@
+export const categories = [
+  {
+    id:"1",
+    img: "https://github.com/user-attachments/assets/053aa27d-7592-4e13-aa31-cdf3cd33c3de",
+    category: "Pasta"
+  },
+  {
+    id:"2",
+    img: "https://github.com/user-attachments/assets/383ae524-4dcf-49bd-8e50-f21b61f3d934",
+    category: "Pizza"
+  },
+  {
+    id:"3",
+    img: "https://github.com/user-attachments/assets/95a3d3a5-af93-44f9-b1fb-b147fb88138a",
+    category: "Vegan"
+  },
+  {
+    id:"4",
+    img: "Desserts",
+    category: "https://github.com/user-attachments/assets/9a2c7487-f3a3-4ad3-9c0e-611cbb29a4d3"
+  },
+  {
+    id:"5",
+    img: "https://github.com/user-attachments/assets/ad8baaaf-ee08-43eb-8717-bde1f2eb6d8a",
+    category: "Smoothies"
+  },
+  {
+    id:"6",
+    img: "(https://github.com/user-attachments/assets/ebec33e9-376e-45c9-a5be-0b4671b7a016",
+    category: "Breakfast"
+  }
+]
