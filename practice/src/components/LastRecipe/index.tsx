7f24f160196236d9e3d38e58615ad33b17c4d3de
diepphan <ngocdiepphan180402@gmail.com>
2024-07-21 16:38:22 +0700

    Implement update render last recipe #239

--- practice/src/components/LastRecipe/index.tsx
+++ practice/src/components/LastRecipe/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Recipe } from "type/recipe";
+
 interface LastRecipeProps {
   recipes: Recipe[];
 }
 const LastRecipe = ({recipes}: LastRecipeProps ) => {
+  const lastRecipes = recipes.slice(-24);
+
   return (
     <ul className="grid grid-cols-2 gap-24 md:gap-30 md:grid-cols-3 lg:grid-cols-4">
-      {recipes.map((recipe) => (
+      {lastRecipes.map((recipe) => (
         <li key={recipe.id}>
           <Link to={`/recipe/${recipe.id}`}>
             <article className="flex flex-col gap-16 cursor-pointer border-none text-primary">
