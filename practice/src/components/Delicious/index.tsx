879549c94e800e20f07e79fdd3b47d095036337b
diepphan <ngocdiepphan180402@gmail.com>
2024-07-21 16:19:25 +0700

    Implement update render data for delicious and sweet-tooth #237

--- practice/src/components/Delicious/index.tsx
+++ practice/src/components/Delicious/index.tsx
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Rating from "../RatingStar";
 import { Link } from "react-router-dom";
-import { recipe } from "mocks/recipe";
+import { Recipe } from "type/recipe";
+import RecipeService from "services/recipe";
 
-const Delicious: React.FC = () => {
+interface RecipeList {
+  title: string;
+  id: string;
+  collection: number;
+}
+
+const Delicious = ({ title, id, collection }: RecipeList) => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const recipeService = new RecipeService();
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const recipeResponse = await recipeService.fetchRecipes();
+      if (!recipeResponse.error) {
+        setRecipes((recipeResponse.data as Recipe[]) || []);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
+  const filterRecipes = (collectionId: number) => {
+    return recipes
+      .filter(
+        (recipe) =>
+          recipe.collection_id === collectionId && recipe.ratings === 5,
+      )
+      .slice(0, 3);
+  };
+
   return (
-    <ul className="flex flex-wrap gap-24 md:flex-row md:grid md:grid-cols-3 md:gap-20">
-      {recipe.map((item, index) => (
-        <li key={index} className="flex flex-col gap-10">
-          <Link to={`/recipe/${item.id}`}>
-            <article>
-              <img
-                className="card__image w-full"
-                src={item.img}
-                alt={`Picture of ${item.name}`}
-              />
-              <Rating ratings={item.ratings} />
-              <p className="font-sans text-lg md:text-xl">{item.name}</p>
-            </article>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <section className="mx-12 md:mx-102 lg:mx-30">
+      <h3 className="text-2xl mt-37 mb-35 font-serif md:text-4xl md:mt-60">
+        {title}
+      </h3>
+      <ul
+        className="flex flex-wrap gap-24 md:flex-row md:grid md:grid-cols-3 md:gap-20"
+        id={id}
+      >
+        {filterRecipes(collection).map((item, index) => (
+          <li key={index} className="">
+            <Link to={`/recipe/${item.id}`}>
+              <article className="flex flex-col gap-13">
+                <img
+                  className="card__image w-full"
+                  src={item.img}
+                  alt={`Picture of ${item.name}`}
+                />
+                <Rating ratings={item.ratings} />
+                <p className="font-sans text-lg md:text-xl">{item.name}</p>
+              </article>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
