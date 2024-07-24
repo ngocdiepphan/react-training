e6bc25c3f59a6ed9244ba1dbbe3f3cc92231680d
diepphan <ngocdiepphan180402@gmail.com>
2024-07-24 10:34:05 +0700

    Implement move the property to constant #263

---
+++ practice/src/constants/index.ts
@@ -0,0 +1,28 @@
+export const FORM_MODAL = [
+  { label: "Image", type: "text", id: "img", name: "img" },
+  { label: "Name", type: "text", id: "name", name: "name" },
+  { label: "Creator", type: "text", id: "creator", name: "creator" },
+  { label: "Description", type: "text", id: "description", name: "description"},
+  { label: "Creation Date", type: "date", id: "createdAt", name: "createdAt"},
+];
+
+export const NAV_BAR_LIST = [
+  {
+    title: "User",
+    type: "user",
+    icon: "user-item",
+    className: "bg-user-item",
+  },
+  {
+    title: "Recipes",
+    type: "recipe",
+    icon: "recipe-item",
+    className: "bg-recipe-item",
+  },
+];
+
+export const CATEGORY = [
+  { title: "Super Delicious", id: "delicious-recipes", collection: 4 },
+  { title: "Sweet Tooth", id: "sweet-recipes", collection: 3 },
+];
+
