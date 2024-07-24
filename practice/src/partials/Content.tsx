897ce3ec40d7f69d5b9338684ce7e00544d4e13f
diepphan <ngocdiepphan180402@gmail.com>
2024-07-24 14:25:24 +0700

    Implement create component chip #256

--- practice/src/partials/Content.tsx
+++ practice/src/partials/Content.tsx
@@ -5,6 +5,7 @@ import { Creator, CreationDate, CommentsCount, RatingStar } from "components";
 
 // Type
 import { Recipe } from "type/recipe";
+import Chip from "components/Chip";
 
 interface ContentProps {
   recipeData: Recipe;
@@ -15,12 +16,7 @@ const Content = ({ recipeData }: ContentProps) => {
     <section className="mx-12 md:mx-102 lg:mx-30">
       <h4 className="font-serif text-2xl md:text-6xl">{recipeData.name}</h4>
       <div className="flex flex-col mt-24 pb-37 border-b border-b-borderPrimary hover:border-b-primary md:flex-row md:pb-37">
-        <ul className="flex flex-row gap-24 items-center">
-          <Creator recipe={recipeData} />
-          <CreationDate recipe={recipeData} />
-          <CommentsCount count={recipeData.comment} />
-          <RatingStar ratings={recipeData.ratings} />
-        </ul>
+        <Chip recipeData={recipeData} />
       </div>
       <p className="font-sans mt-24 mb-35 text-xs md:text-2xl lg:text-xs">
         {recipeData.description}
