663ab88fdf0d1a9c5b423ad41d9cdbd746311e63
diepphan <ngocdiepphan180402@gmail.com>
2024-07-27 20:44:51 +0700

    Update render category #283

--- practice/src/components/Collection/Collection.stories.tsx
+++ practice/src/components/Collection/Collection.stories.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { StoryFn, Meta } from "@storybook/react";
 
+// Component
 import { Collection } from "components";
 
+// Type
+import { CollectionProps } from "components/Collection";
 export default {
   title: "Components/Collection",
   component: Collection,
 } as Meta;
 
-const Template: StoryFn = (args) => <Collection {...args} />;
+const Template: StoryFn<CollectionProps> = (args) => <Collection {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  id: "2",
+  name: "Fancy Glazed Dounts",
+  img: "https://github.com/user-attachments/assets/d12045b1-9820-4fa9-9a85-23f66ff92071",
+  recipes: 30,
+};
--- practice/src/partials/Collection.tsx
+++ practice/src/partials/Collection.tsx
@@ -3,14 +3,25 @@ import React from "react";
 // Component
 import { Collection } from "components";
 
+// Mock
+import { collections } from "mocks/collection";
 const CollectSection: React.FC = () => {
   return (
     <section className="mx-12 md:mx-102 lg:mx-30">
       <h3 className="text-2xl md:text-medium font-serif mt-60 mb-35">
         Hand-Picked Collections
       </h3>
-      <ul className="w-full">
-        <Collection />
+      <ul className="w-full grid grid-cols-1 gap-22 md:gap-30 md:grid-cols-2">
+        {collections.map((item) => (
+          <li key={item.id}>
+            <Collection
+              id={item.id}
+              name={item.name}
+              img={item.img}
+              recipes={item.recipes}
+            />
+          </li>
+        ))}
       </ul>
     </section>
   );
