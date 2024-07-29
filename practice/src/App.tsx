18b80de94d4be45fd552bd476024162cc9085505
diepphan <ngocdiepphan180402@gmail.com>
2024-07-29 10:36:18 +0700

    Update data json #292

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -36,11 +36,11 @@ function App() {
         <Route path="/sign-in" element={<SignInForm />} />
         <Route path="/sign-up" element={<SignUpForm />} />
         <Route
-          path="/homepage"
+          path="/homepage/*"
           element={<ProtectRoute element={<HomePage />} />}
         />
         <Route
-          path="/dashboard"
+          path="/dashboard/*"
           element={<ProtectRoute element={<Dashboard />} />}
         />
         <Route
